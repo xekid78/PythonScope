a0ed40a7f95a51601b91b7b34040853b71de6dd5
Makoto Tanabe <xekid78@outlook.com>
2018-03-10 14:33:40 +0900

    Add files via upload

---
+++ scope.py
@@ -0,0 +1,11 @@
+def sum(x, y):
+    a = 5
+    print("変数ａは" + str(a) + "です。")
+    return x + y
+
+a = 10
+b = 20
+gokei = sum(a, b)
+print("変数ａは" + str(a) + "です。")
+print("変数bは" + str(b) + "です。")
+print("合計は" + str(gokei) + "です。")
